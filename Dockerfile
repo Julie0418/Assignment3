606c2077f8820ff4a2251d77f33fe93d660b0e45
Julie0418 <khatoonjulekha4@gmail.com>
2025-04-10 22:37:48 -0400

    port changed

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,7 @@ COPY . /app
 RUN pip install --no-cache-dir flask flask-cors
 
 # Expose port 5000
-EXPOSE 5000
+EXPOSE 8080
 
 # Run the application
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -4,4 +4,5 @@ app = Flask(__name__)
 def hello_cloud():
     return 'Hello from khatoon ECS Container!'
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
+
